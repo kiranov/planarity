e817af93908e784a23b24dd5591c301073ac5c53
vovaf709 <32431463+vovaf709@users.noreply.github.com>
2019-05-20 15:25:24 +0300

    Update 1.cxx
    edges-segments

--- 1.cxx
+++ 1.cxx
@@ -29,9 +29,8 @@ public:
 		return ret;
 	}
 	void del(){
-		int v;
 		while(current != 0){
-			v = pop();
+			pop();
 		}
 	}
 	int val(){
@@ -69,13 +68,13 @@ public:
 		}*/
 		//stack s(size);
 	}
-	~Graph(){
+	/*~Graph(){
 		for(size_t i = 0; i < size; i++){
 			delete []matrix[i];
 		}
 		delete []matrix;
 		delete []colour;
-	}
+	}*/
 	void print(){
 		for(size_t i = 0; i < size; i++){
 			for(size_t j = 0; j < size; j++){
@@ -133,13 +132,62 @@ public:
 	}
 };
 
+vector<Graph> segments(Graph G, vector<int> cycle){
+	Graph Z(G.size);
+	vector<Graph> seg;
+	seg.reserve(30);
+	for (size_t i = 0; i < 20; i++){
+		seg.push_back(Z);
+	}
+	int count = 0;
+	cycle.push_back(cycle.front());
+	for(size_t i = 0; i < G.size; i++){
+		int index = -1;
+		for(size_t j = 0; j < cycle.size(); j++){
+			if(cycle[j] == i){
+				index = j;
+				break;
+			}
+		}
+		if(index >= 0){
+			for(size_t j = 0; j < cycle.size() - 1; j++){
+				if(G.matrix[i][cycle[j]] == 1){
+					if(index == 0) {
+						if((cycle[1] != cycle[j]) && (cycle[cycle.size() - 2] != cycle[j])) {
+							G.matrix[i][cycle[j]] = 0;
+							G.matrix[cycle[j]][i] = 0;
+							Graph G_1(G.size);
+							G_1.AddEdge(i, cycle[j]);
+							seg[count] = G_1;
+							count++;
+						}
+					}
+					else {
+						if((cycle[index + 1] != cycle[j]) && (cycle[index - 1] != cycle[j])) {
+							G.matrix[i][cycle[j]] = 0;
+							G.matrix[cycle[j]][i] = 0;
+							Graph G_1(G.size);
+							G_1.AddEdge(i, cycle[j]);
+							seg[count] = G_1;
+							count++;
+						}
+					}
+				}
+			}
+		}
+	}
+	while(seg.size() > count){
+		seg.pop_back();
+	}
+	return seg;
+}
+
 int main(int argc, char **argv)
 {
-	/*Graph A(7);
+	Graph A(7);
 	A.AddEdge(0, 1);
 	A.AddEdge(0, 3);
 	A.AddEdge(0, 5);
-	A.AddEdge(0, 2);
 	A.AddEdge(1, 2);
 	A.AddEdge(1, 3);
 	A.AddEdge(1, 4);
@@ -149,12 +197,12 @@ int main(int argc, char **argv)
 	A.AddEdge(4, 6);
 	A.AddEdge(4, 5);
 	A.AddEdge(5, 6);
-	Graph B(3);
+	/*Graph B(3);
 	B.AddEdge(0,1);
 	B.AddEdge(1, 2);
 	B.AddEdge(2, 0);
 	
-	B.print();*/
+	B.print();
 	Graph C(8);
 	C.AddEdge(0,1);
 	C.AddEdge(0,6);
@@ -164,20 +212,37 @@ int main(int argc, char **argv)
 	C.AddEdge(3,4);
 	C.AddEdge(4,5);
 	C.AddEdge(5,2);
-	
-	stack s(8);
+	*/
+	Graph D(6);
+	D.AddEdge(0, 1);
+	D.AddEdge(1, 2);
+	D.AddEdge(2, 3);
+	D.AddEdge(3, 4);
+	D.AddEdge(4, 5);
+	D.AddEdge(5, 0);
+	D.AddEdge(2, 5);
+	D.AddEdge(1, 5);
+	stack s(10);
 	/*s.push(0);
 	s.push(1);
 	int a = s.val();
 	cout << a << endl;
 	a = s.pop();
 	cout << a;*/
-	C.find_cycle(0,&s);
+	D.find_cycle(0,&s);
 	vector<int> cycl;
-	cycl = C.cycle(&s);
+	cycl = D.cycle(&s);
 	for(size_t i = 0; i < cycl.size(); i++){
 		cout << cycl[i] << " ";
 	}
+	printf("\n");
+	//vector<Graph> segmenti = segments(A, cycl);
+	//printf("%d", segmenti.size());
+	vector <Graph> segmenti;
+	segmenti = segments(D, cycl);
+	for (size_t i = 0; i < segmenti.size(); i++){
+		segmenti[i].print();
+		printf("\n");
+	}
 	return 0;
 }
-
